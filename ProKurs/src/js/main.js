8a2d6ed0029e4918d918dfb331ad82d8ef501283
yurgent <yurgent.gu@gmail.com>
2016-10-26 16:16:21 +0300

    added prokurs

---
+++ ProKurs/src/js/main.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+
+
+  //header will take all screen (min-width: 640px)
+  function hResize() {
+    $('#header').css('min-height', $(window).height());
+  };
+  $(window).resize(function() {
+    hResize();
+  });
+
+  //tabs top_line
+  $('.top_phone .wrapper .tab').click(function() {
+    $('.top_phone .wrapper .tab').removeClass('active').eq($(this).index()).addClass('active');
+    $('.top_phone .tab_item').hide().eq($(this).index()).fadeIn()
+  }).eq(0).addClass('active');
+
+  //tabs_header
+  $('.tabs_header .wrapper .tab').click(function() {
+    $('.tabs_header .wrapper .tab').removeClass('active').eq($(this).index()).addClass('active');
+    $('.tabs_header .tab_item').hide().eq($(this).index()).fadeIn()
+  }).eq(0).addClass('active');
+
+  //tabs .contacts_top
+  $('.contacts_top .tab').click(function() {
+    $('.contacts_top .tab').removeClass('active').eq($(this).index()).addClass('active');
+    $('.s_contacts .tab_item').hide().eq($(this).index()).fadeIn()
+  }).eq(0).addClass('active');
+
+  $(".bottom_phone .wrapper .tab").click(function() {
+    $(".bottom_phone .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+    $(".bottom_phone .tab_item").hide().eq($(this).index()).fadeIn()
+  }).eq(0).addClass("active");
+
+  //parallax effect
+  $('.s_review').parallax({imageSrc: '../img/middle_bg.jpg'});
+
+  //carousel
+  $(".owl-carousel").owlCarousel({
+    dots : true, // true for pagination
+    responsive : {
+      0 : {
+        items : 1,
+        nav : true
+      }
+    },
+    navText : ""
+  });
+
+  $(".content_items").addClass("animated zoomIn");
+
+});
